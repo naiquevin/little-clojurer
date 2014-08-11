3e840ac40538b15fefd1bae1e4a96e635d311393
Vineet Naik <vineet@helpshift.com>
2014-08-11 21:20:18 +0530

    Add some more exercises in chapter 5

--- src/little_clojurer/full_of_stars.clj
+++ src/little_clojurer/full_of_stars.clj
@@ -88,3 +88,91 @@
 ;;  (((pecker chuck)))
 ;;  (if (a) ((wood pecker chuck)))
 ;;  could pecker chuck wood)
+
+
+(defn subst*
+  "Substitutes all occurrences of `old` with `new` value in nested
+  list `xs`."
+  [new old xs]
+  (when (seq xs)
+    (if (not (coll? (first xs)))
+      (if (= (first xs) old)
+        (conj (subst* new old (rest xs)) new)
+        (conj (subst* new old (rest xs)) (first xs)))
+      (conj (subst* new old (rest xs))
+            (subst* new old (first xs))))))
+
+
+;; > (subst* 'orange 'banana '((banana)
+;;                             (split ((((banana ice)))
+;;                                     (cream (banana))
+;;                                     sherbet))
+;;                             (banana)
+;;                             (bread)
+;;                             (banana brandy)))
+;; ((orange)
+;;  (split ((((orange ice)))
+;;          (cream (orange))
+;;          sherbet))
+;;  (orange)
+;;  (bread)
+;;  (orange brandy))
+
+
+(defn insert-l*
+  "Inserts `new` to the left of all occurrences of `old` in the nested
+  list `xs`."
+  [new old xs]
+  (when (seq xs)
+    (if (not (coll? (first xs)))
+      (if (= (first xs) old)
+        (conj (insert-l* new old (rest xs)) old new)
+        (conj (insert-l* new old (rest xs)) (first xs)))
+      (conj (insert-l* new old (rest xs))
+            (insert-l* new old (first xs))))))
+
+
+;; > (insert-l* 'pecker 'chuck '((how much (wood)) could
+;;                               ((a (wood) chuck))
+;;                               (((chuck)))
+;;                               (if (a) ((wood chuck)))
+;;                               could chuck wood))
+;;   ((how much (wood)) could
+;;    ((a (wood) pecker chuck))
+;;    (((pecker chuck)))
+;;    (if (a) ((wood pecker chuck)))
+;;    could pecker chuck wood)
+
+
+(defn member*
+  "Checks if the atom `a` is present in nested list `xs`."
+  [a xs]
+  (and (not (empty? xs))
+       (if (not (coll? (first xs)))
+         (or (= (first xs) a) (member* a (rest xs)))
+         (or (member* a (first xs)) (member* a (rest xs))))))
+
+;; > (member* 'chips '((potato) (chips ((with) fish) (chips))))
+;;   true
+;; > (member* 'potato '((potato) (chips ((with) fish) (chips))))
+;;   true
+;; > (member* 'onion '((potato) (chips ((with) fish) (chips))))
+;;   false
+
+
+(defn leftmost
+  "Returns the first atomic value in nested list `xs`."
+  [xs]
+  (when (seq xs)
+    (if (not (coll? (first xs)))
+      (first xs)
+      (leftmost* (first xs)))))
+
+;; > (leftmost '((potato) (chips ((with) fish) (chips))))
+;;   potato
+;; > (leftmost '(((hot) (tuna (and))) cheese))
+;;   hot
+;; > (leftmost '(((() four)) 17 (seventeen)))
+;;   nil
+;; > (leftmost (quote ()))
+;;   nil
